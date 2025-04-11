bd8ac7e234c06e93e4735650f3c73999adb13da6
Matheus Bozetti <matheusbzt@gmail.com>
2025-04-11 16:06:37 -0400

    🚀 fix deploy

--- packages/resolvers/dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { UIKitIconVueResolver } from "./vue-resolver";
--- packages/vue-icons/dist/IconSize.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const IconSizeOptions: readonly ["is-16", "is-20", "is-24", "is-32", "is-40", "is-48", "is-52"];
-export type IconsSize = (typeof IconSizeOptions)[number];
