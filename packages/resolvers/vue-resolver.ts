51b31919d0f76974a7b80f6d290137f6f7904aa8
Matheus Bozetti <matheusbzt@gmail.com>
2025-04-11 15:30:15 -0400

    🩹 fix resolver

--- packages/resolvers/vue-resolver.ts
+++ packages/resolvers/vue-resolver.ts
@@ -3,15 +3,12 @@
  * Docs: https://github.com/unplugin/unplugin-vue-components
  */
 export function UIKitIconVueResolver() {
-  console.log("installed");
   return {
     type: "component" as "component" | "directive",
     resolve: (name: string) => {
       if (name.startsWith("UI")) {
-        console.log("import from ", name);
         return { name, from: `@tbdc-agro-softwares/ui-kit-icons/vue` };
       }
-      return name;
     },
   };
 }
