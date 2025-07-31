6069177e02966ec23455b3d4b063390c73bf97d8
Mauro Oliveira <mauroviniciussilva@gmail.com>
2025-07-31 15:30:46 -0300

    🔨 fix resolver extension

--- packages/resolvers/fix.extension.js
+++ packages/resolvers/fix.extension.js
@@ -3,14 +3,17 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 (function () {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
 
-  const pathToFile = join(__dirname, 'dist', 'index.js');
+	const pathToFile = join(__dirname, 'dist', 'index.js');
 
-  const file = readFileSync(pathToFile);
+	const file = readFileSync(pathToFile);
 
-  const newFile = file.toString().replace('vue-resolver', 'vue-resolver.js');
+	const newFile = file
+		.toString()
+		.replace('vue-resolver', 'vue-resolver.js')
+		.replace('react-resolver', 'react-resolver.js');
 
-  writeFileSync(pathToFile, newFile);
+	writeFileSync(pathToFile, newFile);
 })();
