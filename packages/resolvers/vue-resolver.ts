d153ede6bbcf62091780f22197d45846133cfa25
Matheus Bozetti <matheusbzt@gmail.com>
2025-08-13 09:24:27 -0400

    ✨ update vue-plugin tree-shaking

--- packages/resolvers/vue-resolver.ts
+++ packages/resolvers/vue-resolver.ts
@@ -3,12 +3,16 @@
  * Docs: https://github.com/unplugin/unplugin-vue-components
  */
 export function UIKitIconVueResolver() {
-	return {
-		type: 'component' as 'component' | 'directive',
-		resolve: (name: string) => {
-			if (name.startsWith('UI')) {
-				return { name, from: `@tbdcagro/ui-kit-icons/vue` };
-			}
-		},
-	};
+  return {
+    type: "component" as "component" | "directive",
+    resolve: (name: string) => {
+      if (name.startsWith("UI")) {
+        return {
+          from: `@tbdcagro/ui-kit-icons/vue/${name}`,
+          name: "default",
+          as: name,
+        };
+      }
+    },
+  };
 }
--- packages/vue-icons/vite.config.ts
+++ packages/vue-icons/vite.config.ts
@@ -1,28 +1,41 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import path, { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    lib: {
-      entry: [resolve(__dirname, "src/index.ts")],
-      name: "VueIcons",
-      formats: ["es", "cjs"],
-      fileName: (format) => (format === "es" ? "index.mjs" : "index.cjs"),
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        exports: "named",
-        globals: { vue: "Vue" },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        additionalData: [],
       },
     },
-    cssCodeSplit: false,
   },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        preserveModules: true,
+        exports: "named",
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name.replace(/\.vue$/, "") + ".js";
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "_main.css") return "css/index.css";
+          return assetInfo.name || "";
+        },
+        globals: { vue: "Vue" },
+      },
     },
   },
 });
